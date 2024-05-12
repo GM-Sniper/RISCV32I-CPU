67349536c7ca9b69b0870fe7c9a5943d431f7c80
Omar-Ganna <144115958+Kancipher@users.noreply.github.com>
2024-05-12 20:24:34 +0300

    Added the Control Unit module

--- ALU.v
+++ ALU.v
@@ -6,27 +6,27 @@
 * Author: name and email
 * Description: put your description here
 *
-* Change history: 01/01/17 – Did something
-* 10/29/17 – Did something else
+* Change history: 01/01/17 ï¿½ Did something
+* 10/29/17 ï¿½ Did something else
 *
 ************************/
 module ALU (
     input [31:0] A, B,
-    input [3:0] ALUOp,  // {instruction[30],instruction[14:12]}
+    input [3:0] ALUControl,  // {instruction[30],instruction[14:12]}
     output reg [31:0] Result,
     output  ZFlag,  NFlag, reg CFlag, reg OFlag
 );
 
-parameter ADD = 4'b0000;
-parameter SUB = 4'b1001;  
-parameter AND = 4'b0111; 
-parameter OR = 4'b0110;
-parameter XOR = 4'b0100;
-parameter SLL = 4'b0001;
-parameter SRL = 4'b0101;
-parameter SRA = 4'b1101;
-parameter SLT = 4'b0010;
-parameter SLTU = 4'b0011;
+localparam  ADD = 4'b0000;
+localparam  SUB = 4'b1001;  
+localparam  AND = 4'b0111; 
+localparam  OR = 4'b0110;
+localparam  XOR = 4'b0100;
+localparam  SLL = 4'b0001;
+localparam  SRL = 4'b0101;
+localparam  SRA = 4'b1101;
+localparam  SLT = 4'b0010;
+localparam  SLTU = 4'b0011;
 
 assign ZFlag = (Result == 32'b0);
 assign NFlag = (Result[31] == 1); 
@@ -50,7 +50,7 @@ always @ (*) begin
         SRL: Result = A >> B; 
         SRA: Result = A >>> B; 
         SLT: Result = (A < B) ? 1 : 0; 
-        SLTU: Result = (A < B) ? 1 : 0; 
+        SLTU: Result = (A < B) ? 1 : 0;//manage unsigned 
         default: Result = 32'b0; 
     endcase
 end
---
+++ ControlUnit.v
@@ -0,0 +1,141 @@
+`timescale 1ns / 1ps
+/***********************
+*
+* Module: Controlunit.v
+* Project: RISCV_CPU
+* Author: Omar Ahmed Ganna, omarganna@aucegypt.edu
+* Description: This module is the control unit of the RISCV CPU
+*
+* Change history: 12/05/24
+
+************************/
+
+
+module ControlUnit (
+    input [6:0] opcode,
+    output reg [3:0] ALUControl,
+    output reg RegWrite,//
+    output reg MemtoReg,//
+    output reg MemWrite,//
+    output reg MemRead,//
+    output reg Branch,//
+    output reg [1:0] Jump,//
+    output reg  ALUSrc,
+    output reg LUI,
+    output reg AUIPC,
+);
+
+always @(*) begin
+    case(opcode[6:2])   
+        5'b01100: begin //R-type
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 0;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+        5'b00100: begin //I-type
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+        5'b11000: begin //SB-type
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 1;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+        5'b00000: begin //U-type
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            RegDst <= 2'b10;
+            LUI <= 1;
+            AUIPC <= 0;
+        end
+        5'b01000: begin //S-type
+            // ALUControl = 4'b0000;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 1;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+        5'b01101: begin 
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            LUI <= 1;
+            AUIPC <= 0;
+        end
+        5'b00101: begin 
+            // ALUCon<trol = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b00;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 1;
+        end
+        5'b11011: begin 
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b01;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+        5'b11001: begin 
+            // ALUControl = 4'b0111;
+            RegWrite <= 1;
+            MemtoReg <= 0;
+            MemWrite <= 0;
+            MemRead <= 0;
+            Branch <= 0;
+            Jump <= 2'b10;
+            ALUSrc <= 1;
+            LUI <= 0;
+            AUIPC <= 0;
+        end
+endcase
+end
+endmodule
