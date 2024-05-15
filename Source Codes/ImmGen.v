d9176a7f395cee79759ee040556da2a71bfe6c1b
Ramy Shehata <144553627+GM-Sniper@users.noreply.github.com>
2024-05-15 18:03:59 +0300

    Update ImmGen.v

--- Source Codes/ImmGen.v
+++ Source Codes/ImmGen.v
@@ -8,8 +8,8 @@
 * Description: This module is the immediate generator for getting the immediate from the instruction
 *
 * Change history: 13/05/24-> finished the immediate Generator with no default case
-		  14/05/24-> Added default case
-
+		14/05/24-> Added default case
+        15/05/24-> Added the sign extend bit
 ************************/
 module ImmGen(input [31:0] instructions, output reg [31:0] immediate);
 wire [6:0] opcode;
@@ -19,17 +19,17 @@ case(opcode)
 3:  immediate={20'd0,instructions[31:20]};
 19: begin 
     if(instructions [14:12]==3'b001 ||instructions [14:12]==3'b101)   
-        immediate={27'd0,instructions[24:20]};
+        immediate={{27{instructions[24]}},instructions[24:20]};
     else
-    immediate={20'd0,instructions[31:20]};
+    immediate={{20{instructions[31]}},instructions[31:20]};
 
 end
-23: immediate={12'd0,instructions[31:12]};
-35: immediate={20'd0,instructions[31:25],instructions[11:7]};
-55: immediate ={12'd0,instructions[31:12]};
-99: immediate={20'd0,instructions[31],instructions[7],instructions[30:25],instructions[11:8]};
-103: immediate={20'd0,instructions[31:20]};
-111: immediate={12'd0,instructions[31],instructions[19:12],instructions[20],instructions[30:21]};
+23: immediate={{12{instructions[31]}},instructions[31:12]};
+35: immediate={{20{instructions[31]}},instructions[31:25],instructions[11:7]};
+55: immediate ={{20{instructions[31]}},instructions[31:12]};
+99: immediate={{20{instructions[31]}},instructions[31],instructions[7],instructions[30:25],instructions[11:8]};
+103: immediate={{20{instructions[31]}},instructions[31:20]};
+111: immediate={{12{instructions[31]}},instructions[31],instructions[19:12],instructions[20],instructions[30:21]};
 default: immediate=32'b0;
 endcase
 end
